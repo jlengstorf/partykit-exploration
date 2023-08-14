7b615ee04301cf7ce9a2b603a3fd477f95e0d205
Sunil Pai <threepointone@gmail.com>
2023-08-14 21:46:14 +0100

    opt in to the "hibernation" api mode
    This witches the partykit server to use the hibernation mode which scales a lot better (1000s in a room, or so sunil claims)

--- server/index.ts
+++ server/index.ts
@@ -1,41 +1,39 @@
 import type {
-	PartyKitConnection,
-	PartyKitRoom,
-	PartyKitServer,
+  PartyKitConnection,
+  PartyKitRoom,
+  PartyKitServer,
 } from 'partykit/server';
 
 function sendWithWebSocket(_ws: PartyKitConnection, room: PartyKitRoom) {
-	return (message: { type: string; data: object }) => {
-		room.broadcast(
-			JSON.stringify({
-				...message,
-				room: {
-					id: room.id,
-					connections: room.connections.size,
-				},
-			}),
-		);
-	};
+  return (message: { type: string; data: object }) => {
+    room.broadcast(
+      JSON.stringify({
+        ...message,
+        room: {
+          id: room.id,
+          connections: room.connections.size,
+        },
+      })
+    );
+  };
 }
 
 export default {
-	onConnect(ws, room) {
-		ws.addEventListener('message', (event) => {
-			const send = sendWithWebSocket(ws, room);
-			const { type, data } = JSON.parse(event.data as string);
+  onMessage(message, ws, room) {
+    const send = sendWithWebSocket(ws, room);
+    const { type, data } = JSON.parse(message as string);
 
-			switch (type) {
-				case 'ping':
-					send({ type: 'ping', data: { message: 'pong' } });
-					break;
+    switch (type) {
+      case 'ping':
+        send({ type: 'ping', data: { message: 'pong' } });
+        break;
 
-				case 'reaction':
-					send({ type: 'reaction', data: { type: data.type } });
-					break;
+      case 'reaction':
+        send({ type: 'reaction', data: { type: data.type } });
+        break;
 
-				default:
-					send({ type, data });
-			}
-		});
-	},
+      default:
+        send({ type, data });
+    }
+  },
 } satisfies PartyKitServer;
