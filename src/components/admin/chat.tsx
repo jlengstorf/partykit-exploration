288b1c700b3405c3c8feae9de56b272337f787f7
Jason Lengstorf <jason@lengstorf.com>
2023-08-22 11:53:28 -0700

    fix: lol

---
+++ src/components/admin/chat.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import { init, send } from '../../util/partykit';
+import styles from './chat.module.css';
+
+export const Chat = () => {
+	const [chat, setChat] = useState<Array<any>>([]);
+
+	useEffect(() => {
+		init({
+			log: (msg) => {
+				console.log(msg);
+			},
+			onMessage({ type, data }) {
+				if (type !== 'chat') {
+					return;
+				}
+
+				setChat((messages) => [...messages, { data }]);
+			},
+		});
+	}, []);
+
+	return (
+		<>
+			<button
+				onClick={() => {
+					send({
+						type: 'remove-highlight',
+						data: {},
+					});
+				}}
+			>
+				hide highlighted chat
+			</button>
+			<ul className={styles.chat}>
+				{chat.map(({ data }) => {
+					return (
+						<>
+							<li>
+								<p>
+									<strong>{data.meta['display-name']}: </strong> {data.message}
+								</p>
+								<div className={styles.actions}>
+									<button
+										onClick={() => {
+											send({
+												type: 'highlight',
+												data: {
+													name: data.meta['display-name'],
+													message: data.message,
+												},
+											});
+										}}
+									>
+										highlight
+									</button>
+								</div>
+							</li>
+						</>
+					);
+				})}
+			</ul>
+		</>
+	);
+};
